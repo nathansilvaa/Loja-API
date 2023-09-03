67745a9116e94e206dd540270d8e50d20d9b7fcf
Nathan Aguiar Silva <nathansilvaxis@gmail.com>
2023-09-03 13:40:14 -0300

    adicionando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+
 
 function App() {
+  const [produtos, setProdutos] = useState([])
+  const url ='https://fakestoreapi.com/products'
+
+  useEffect(()=>{
+    axios.get(url)
+    .then(resposta=>{
+      setProdutos(resposta.data)
+    })
+    .catch(error =>{
+      console.log(error)
+    });
+  },[]);
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Minha loja</h1>
+      <ul> 
+        {produtos.map(produto => (
+          <li key = {produto.id}>
+            <h2>{produto.title}</h2>
+            <img src={produto.image} alt={produto.title}/>
+            <div className='div'>
+              <p>R${produto.price}</p>
+              <button>Adicionar ao carrinho</button>
+            </div>
+          </li> 
+        ))}
+      </ul>
     </div>
   );
 }
